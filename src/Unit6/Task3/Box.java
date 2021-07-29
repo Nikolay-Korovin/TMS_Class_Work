8f25b45f9350d006730cd711c3ecd14370dec927
Nikolay Korovin <squiblawarcurse@gmail.com>
2021-07-29 13:31:25 +0300

    Unit6 fixes

--- src/Unit6/Task3/Box.java
+++ src/Unit6/Task3/Box.java
@@ -1,7 +1,12 @@
 package Unit6.Task3;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Box extends Shape {
-    Shape[] boxArray;
+    private final int DEFAULT_SIZE = 10;
+    Shape[] boxArray = new Shape[DEFAULT_SIZE];
+
     String name = "Box";
 
     Box(double volume) {
@@ -13,12 +18,36 @@ public class Box extends Shape {
     }
 
     boolean add(Shape... shape) {
-        boxArray = new Box[shape.length];
+        int counter2 = 0;
+        int counter = 0;
+
+        for (int i = counter2; i < boxArray.length; i++) {
+            if (boxArray[i] != null) {
+                counter2++;
+            }
+        }
+        if (counter2 == boxArray.length - 5) {
+            boxArray = Arrays.copyOf(boxArray, boxArray.length * 2);
+        }
+        if (shape.length >= boxArray.length && volume > 0) {
+            boxArray = Arrays.copyOf(boxArray, boxArray.length * 2);
+        }
         for (int i = 0; i < boxArray.length; i++) {
-            if (volume >= shape[i].getVolume()) {
-                volume = volume - shape[i].getVolume();
-                System.out.println("Впихиваем " + shape[i].getName() + " обьем = " + shape[i].getVolume());
-                System.out.println("Оставшийся обьём коробки = " + volume);
+            if (boxArray[i] != null) {
+                counter++;
+            } else {
+                break;
+            }
+        }
+
+        if (volume > 0) {
+            for (int i = 0; i < shape.length; i++) {
+                if(shape[i].getVolume() < volume) {
+                    System.out.println("Запихиваем в коробку " + shape[i].getName() + " обьёмом " + shape[i].getVolume());
+                    volume = volume - shape[i].getVolume();
+                    boxArray[i + counter] = shape[i];
+                    System.out.println("в коробке осталось " + getVolume());
+                }
             }
         }
         if (volume > 0) {
--- src/Unit6/Task3/Test.java
+++ src/Unit6/Task3/Test.java
@@ -1,8 +1,19 @@
 package Unit6.Task3;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
-        Box box = new Box(1400);
-        box.add(new Pyramid(10,15), new Cylinder(100), new Box(100));
+        int counter = 0;
+        Box box = new Box(500);
+        box.add(new Pyramid(10,15), new Cylinder(10), new Pyramid(10));
+        for (int i = 0; i < 41; i++) {
+            box.add(new Pyramid(10));
+        }
+        box.add(new Pyramid(10,15), new Cylinder(15), new Pyramid(14));
+
+        for (int i = 0; i < box.boxArray.length; i++) {
+            System.out.println(box.boxArray[i] + " " + ++counter);
+        }
     }
 }
